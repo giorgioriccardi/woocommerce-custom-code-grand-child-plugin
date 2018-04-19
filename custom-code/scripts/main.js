f5b088e12d4e45d2532aa1874ffe9f8cc75b4edc
Giorgio Riccardi <griccardi@my.bcit.ca>
2018-04-19 09:17:03 -0700

    Custom console.log message to avoid confusion with custom theme cl message

--- custom-code/scripts/main.js
+++ custom-code/scripts/main.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function ($) {
   // Custom jQuery goes here
-  console.info('test GRC JS injection');
+  console.info('test: GRC-WC-custom-plugin-2018 jQuery injection');
 });
